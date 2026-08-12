d63190aa3118c869b878e6ff05f9cb4c593131f2
Jandres283 <75290851@iestpmrap.drelm.edu.pe>
2026-08-12 13:39:51 -0500

    Refactor restaurant routes and middleware imports

--- router/restaurant.js
+++ router/restaurant.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const upload = require("../middlewares/multer"); 
+const upload = require('../middlewares/upload'); 
 const RestaurantController = require("../controllers/restaurant");
 const mdAuth = require("../middlewares/authenticated");
 const mdRole = require("../middlewares/isRole");
